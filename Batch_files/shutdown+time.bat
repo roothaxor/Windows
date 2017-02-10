7cc8e36565e956c20b34723c4847efc2c9d4dc97
SanJay <indianblogger786@gmail.com>
2017-02-10 06:13:31 -0800

    Update shutdown+time.bat

--- Batch_files/shutdown+time.bat
+++ Batch_files/shutdown+time.bat
@@ -21,7 +21,7 @@ for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 REM batch program managing the shutdown of computer with time limit 
 REM all you need to do is double click it and put minutes to shutdown.
 title Shutdown PC With Time Limit
-set /p time=Set minutes to shuttle to take off PC's Power?: 
+set /p time=Set minutes for shuttle to take off PC's Power?: 
 set /a time=%time%*60 
 shutdown /a 
 shutdown /s /f /t %time%
